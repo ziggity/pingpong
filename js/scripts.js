c53cec0c3a40336732d029f1261e9dd7616eaf20
zach beecher <zachbeecher@gmail.com>
2017-02-24 13:42:56 -0800

    add photos of pingpong balls to each output, appends, this is looking rediculous, but it's fun to mess around - might have to tweak this to look more professional

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,13 @@
 function pingPong(pingPongs){
   for (var i = 1; i <= pingPongs; i++) {
     if (i % 15 === 0) {
-      $("#outPut").append("<li>ping-pong</li>");
+      $("#outPut").append('<img src="http://83e2u32cf1b4dlzbl29etyxt.wpengine.netdna-cdn.com/wp-content/uploads/2015/09/Spin-Button.jpg"> ping-pong');
     } else if (i % 3 === 0) {
-      $("#outPut").append("<li>ping</li>");
+      $("#outPut").append('<img src="https://cdn.shutterstock.com/shutterstock/videos/23115502/thumb/1.jpg?i10c=img.resize"> ping');
     } else if (i % 5 === 0) {
-      $("#outPut").append("<li>pong</li>");
+      $("#outPut").append('<img src="https://cdn.shutterstock.com/shutterstock/videos/23115493/thumb/1.jpg"> pong');
     } else {
-      $("#outPut").append("<li>" + i + "</li>").slideDown;
+      $("#outPut").append( i + " "+'<img src="http://pngimg.com/upload_small/ping_pong/ping_pong_PNG10373.png">');
     }
   }
 }
