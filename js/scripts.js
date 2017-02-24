9c9d7f6524565e9cecdf8f74371519fd3e453d03
zach beecher <zachbeecher@gmail.com>
2017-02-24 11:38:53 -0800

    logic is good, just need to tweak it so there is some more compute power going on behind the scenes. Right now it's very basic. Add style to the li where ping-pong shows to the user, also switched to a prompt instead of button, I fancy that more for some reason

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,20 @@
-//Backend goes here:
-function pingPongTime()
 
-
-
-
-//Frontend goes below:
-$(function() {
-  $("#userInput").submit(function(event) {
-    event.preventDefault();
-    var playerNum = $("#userInput").val();
-  })
+//backend do some logic to the input
+function pingPong(pingPongs){
+  for (var i = 1; i <= pingPongs; i++) {
+    if (i % 15 === 0) {
+      $("#outPut").append("<li>ping-pong</li>");
+    } else if (i % 3 === 0) {
+      $("#outPut").append("<li>ping</li>");
+    } else if (i % 5 === 0) {
+      $("#outPut").append("<li>pong</li>");
+    } else {
+      $("#outPut").append("<li>" + i + "</li>");
+    }
+  }
+}
+//frontend, gather user's input
+$(function(){
+	var userNumber = parseInt(prompt("¡Enter your digits, por favor!"));
+  var letPing = pingPong(userNumber)
 });
