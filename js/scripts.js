0f496777b48ec9ee56f289bbe6cbbf5b9777d272
zach beecher <zachbeecher@gmail.com>
2017-02-24 09:54:22 -0800

    add background img, some frontend logic to take in user input with submit event, some other styling

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+//Backend goes here:
+function pingPongTime()
+
+
+
+
+//Frontend goes below:
+$(function() {
+  $("#userInput").submit(function(event) {
+    event.preventDefault();
+    var playerNum = $("#userInput").val();
+  })
+});
